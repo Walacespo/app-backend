d4478d66101d9e53ce1f09f570ae0fa7c51158eb
Walace da Silva <walacespo@gmail.com>
2022-02-08 18:18:00 -0300

    Produtos e associoação muitos para muitos

--- src/main/java/com/walace/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/walace/cursomc/resources/CategoriaResources.java
@@ -25,8 +25,6 @@ public class CategoriaResources {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		
-
-		
 		return ResponseEntity.ok().body(obj);
 		
 	}
