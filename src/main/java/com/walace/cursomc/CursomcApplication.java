1594d7fcd81cc95c07d7456d5bcb8a192ee0406f
Walace da Silva <walacespo@gmail.com>
2022-02-12 16:22:50 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/walace/cursomc/CursomcApplication.java
+++ src/main/java/com/walace/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.walace.cursomc.domain.Cidade;
 import com.walace.cursomc.domain.Cliente;
 import com.walace.cursomc.domain.Endereco;
 import com.walace.cursomc.domain.Estado;
+import com.walace.cursomc.domain.ItemPedido;
 import com.walace.cursomc.domain.Pagamento;
 import com.walace.cursomc.domain.PagamentoComBoleto;
 import com.walace.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.walace.cursomc.repositories.CidadeRepository;
 import com.walace.cursomc.repositories.ClienteRepository;
 import com.walace.cursomc.repositories.EnderecoRepository;
 import com.walace.cursomc.repositories.EstadoRepository;
+import com.walace.cursomc.repositories.ItemPedidoRepository;
 import com.walace.cursomc.repositories.PagamentoRepository;
 import com.walace.cursomc.repositories.PedidoRepository;
 import com.walace.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository ItemPedidoRepository;
 	
 	
 	
@@ -125,6 +130,19 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1,p1, 0.00,1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1,p3, 0.00,2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2,p2, 100.00,1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));	
+		
+		ItemPedidoRepository.saveAll(Arrays.asList(ip1, ip2,ip3));
 		
 	}
 	
