67d550006f5096e8b857636a6cdf6c06650547fe
Walace da Silva <walacespo@gmail.com>
2022-02-14 19:01:54 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/walace/cursomc/CursomcApplication.java
+++ src/main/java/com/walace/cursomc/CursomcApplication.java
@@ -71,7 +71,7 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 	
 		Categoria cat1 = new Categoria(null,"Informática");
-		Categoria cat2 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -80,14 +80,19 @@ public class CursomcApplication implements CommandLineRunner{
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		
 	
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		
 		Estado est1 = new Estado(null, "Minas Geris");
 		Estado est2 = new Estado(null, "São Paulo");
