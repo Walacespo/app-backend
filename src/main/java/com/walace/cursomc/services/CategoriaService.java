197a92ee03a5dc7dea0116677d4b9673818a9412
Walace da Silva <walacespo@gmail.com>
2022-02-22 19:13:47 -0300

    Validação síntatica com Bean validation

--- src/main/java/com/walace/cursomc/services/CategoriaService.java
+++ src/main/java/com/walace/cursomc/services/CategoriaService.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.walace.cursomc.domain.Categoria;
+import com.walace.cursomc.dto.CategoriaDTO;
 import com.walace.cursomc.repositories.CategoriaRepository;
 import com.walace.cursomc.services.exception.DataIntegrityException;
 import com.walace.cursomc.services.exception.ObjectNotFoundException;
@@ -54,5 +58,16 @@ public class CategoriaService {
 				return repo.findAll();
 			
 		}
+			
+
+			public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+				PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+				return repo.findAll(pageRequest);
+			}
+			
+			public Categoria fromDTO(CategoriaDTO objDto) {
+				
+				return new Categoria(objDto.getId(), objDto.getNome());
+			}
 		
 }
