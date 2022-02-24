ac0e6b452e93f62b5170c9e1ce24ceb66b1dfb4c
Walace da Silva <walacespo@gmail.com>
2022-02-24 14:23:54 -0300

    Ajustando e testando deleção de cliente

--- src/main/java/com/walace/cursomc/services/ClienteService.java
+++ src/main/java/com/walace/cursomc/services/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 			repo.deleteById(id);
 			}catch (DataIntegrityViolationException e){
 				
-				throw new DataIntegrityException("Não é possível excluir porque há entidades ralacionadas");
+				throw new DataIntegrityException("Não é possível excluir porque há pedidos ralacionadas");
 			}
 		}
 			
