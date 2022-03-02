71d3bb69429005868a871f4a0cc88d073e705af5
Walace da Silva <walacespo@gmail.com>
2022-03-02 10:41:47 -0300

    Implementando toString de Pedido

--- src/main/java/com/walace/cursomc/services/PedidoService.java
+++ src/main/java/com/walace/cursomc/services/PedidoService.java
@@ -11,6 +11,7 @@ import com.walace.cursomc.domain.ItemPedido;
 import com.walace.cursomc.domain.PagamentoComBoleto;
 import com.walace.cursomc.domain.Pedido;
 import com.walace.cursomc.domain.enums.EstadoPagamento;
+import com.walace.cursomc.repositories.ClienteRepository;
 import com.walace.cursomc.repositories.ItemPedidoRepository;
 import com.walace.cursomc.repositories.PagamentoRepository;
 import com.walace.cursomc.repositories.PedidoRepository;
@@ -38,6 +39,9 @@ public class PedidoService {
 	@Autowired
 	private ProdutoService produtoService;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 		public Pedido find(Integer id) {
 			
 			Optional<Pedido> obj = repo.findById(id);
@@ -51,6 +55,7 @@ public class PedidoService {
 		public Pedido insert(Pedido obj) {
 			obj.setId(null);
 			obj.setInstante(new Date());
+			obj.setCliente(clienteService.find(obj.getCliente().getId()));
 			obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 			obj.getPagamento().setPedido(obj);
 			if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -61,11 +66,14 @@ public class PedidoService {
 			pagamentoRepository.save(obj.getPagamento());
 			for (ItemPedido ip : obj.getItens()) {
 				ip.setDesconto(0.0);
-				ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+				ip.setProduto(produtoService.find(ip.getProduto().getId()));
+				ip.setPreco(ip.getProduto().getPreco());
 				ip.setPedido(obj);
 			}
 			itemPedidoRepository.saveAll(obj.getItens());
+			System.out.println(obj);
 			return obj;
+			
 		}
 			
 			
