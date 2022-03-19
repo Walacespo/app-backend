d683964f2e0b20ed598f5d20ab1ebe4df3a77bea
Walace da Silva <walacespo@gmail.com>
2022-03-19 14:30:16 -0300

    Expondo o header location nas respostas

---
+++ src/main/java/com/walace/cursomc/filters/HeaderExposureFilter.java
@@ -0,0 +1,10 @@
+package com.walace.cursomc.filters;
+
+import javax.servlet.Filter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeaderExposureFilter implements Filter{
+
+}
